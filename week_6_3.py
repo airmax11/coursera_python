d38d44a2a9f3528d6ad3e8fb5f818543380f7cd3
airmax11 <maksvellspam@gmail.com>
2016-01-19 23:24:35 +0100

    Updated several1

--- week_6_3.py
+++ week_6_3.py
@@ -1,22 +1,33 @@
- import json
- import urllib
+import json
+import urllib
 
 
- enter_url = raw_input ("Your URL adress or 1 for use your API : ")
+enter_url = raw_input ("Your URL adress or 1 for use your API or 2 for GOOGLE API: ")
+
+#if len(enter_url) < 1 : break
+
+if enter_url == '1':
+	enter_url = 'http://python-data.dr-chuck.net/geojson?'
+
+if enter_url =='2':
+	enter_url = 'http://maps.googleapis.com/maps/api/geocode/json?'
 
 
- if len (enter_url) < 1:
- 	break
 
- elif enter_url == '1':
- 	enter_url = 'http://python-data.dr-chuck.net/geojson'
+address = raw_input ("Enter your location :")
+#if len (adress) < 1 : break
 
+url = enter_url + urllib.urlencode({'sensor':'false', 'address': address})
+print url
 
+url_open = urllib.urlopen (url).read()
 
- adress = raw_input ("Enter your location :")
- if len (adress) < 1 :
- 	break
+js = json.loads(str(url_open))
 
- url = enter_url + urllib.urlencode({'sensor':'false', 'address': address})
+print json.dumps(js, indent = 4)
 
- print url
+location = js['results'][0]['place_id']
+location2 = js['results'][0]['formatted_address']
+
+print location
+print location2
